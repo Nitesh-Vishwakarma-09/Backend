8d957f8a576768cc7d75674919891918dcc45feb
Nitesh Vishwakarma <Nitesh7415663723@gmail.com>
2026-08-21 00:35:54 +0530

    Uncomment file system and HTTP server operations

--- script1st.js
+++ script1st.js
@@ -1,60 +1,59 @@
-// const fs=require('fs');
-// fs.appendFile("hey.txt","main thik hoon",function(err){
-//     if(err) console.log(err);
-//         else console.log("done");
+ const fs=require('fs');
+fs.appendFile("hey.txt","main thik hoon",function(err){
+    if(err) console.log(err);
+         else console.log("done");
         
     
-// })
+ })
 
-// fs.rename("hey.txt","hello.txt",function(err){
-//     if(err) console.log(err);
-//         else console.log("done");
+ fs.rename("hey.txt","hello.txt",function(err){
+     if(err) console.log(err);
+         else console.log("done");
         
     
-// })
+ })
 
-// fs.copyFile("hello.txt","./copy/nitesh.txt",function(err){
-//     if(err) console.log(err);
-//         else console.log("done");
+ fs.copyFile("hello.txt","./copy/nitesh.txt",function(err){
+     if(err) console.log(err);
+         else console.log("done");
         
     
-// })
+ })
 
-// fs.unlink("package.json",function(err){
-//     if(err) console.log(err);
-//         else console.log("done");
+ fs.unlink("package.json",function(err){
+     if(err) console.log(err);
+         else console.log("done");
         
     
-// })
+ })
 
-// fs.rmdir("./nitesh",{recursive:true},function(err){
-//     if(err) console.log(err);
-//         else console.log("done");
+ fs.rmdir("./nitesh",{recursive:true},function(err){
+     if(err) console.log(err);
+         else console.log("done");
         
     
-// })
+ })
 
-// fs.mkdir("./nitesh",{recursive:true},function(err){
-//     if(err) console.log(err);
-//         else console.log("done");
+ fs.mkdir("./nitesh",{recursive:true},function(err){
+     if(err) console.log(err);
+         else console.log("done");
         
     
-// })
+ })
 
-// fs.readFile("hello.txt",'utf-8',function(err,data){
-//     if (err){ console.log(err);
-//         return;
-//     }
-//          console.log(data);
+ fs.readFile("hello.txt",'utf-8',function(err,data){
+     if (err){ console.log(err);
+         return;
+     }
+         console.log(data);
         
-    
-// })
+     })
 
-// const http=require("http")
+const http=require("http")
 
-// const server =http.createServer(function(req,res){
-//     res.end("hello world");
-// })
-// server.listen(3000);
+const server =http.createServer(function(req,res){
+    res.end("hello world");
+ })
+ server.listen(3000);
 
-// console.log("Hello World");
+ console.log("Hello World");
